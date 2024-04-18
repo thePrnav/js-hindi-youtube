8236ef6d82888135cfdab77d7b35fdeb7415411b
thePrnav <114599439+thePrnav@users.noreply.github.com>
2024-04-18 11:47:31 +0000

    Functions with objects and array

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -37,4 +37,40 @@ function loginUserMessage(username = "sam"){
 
 // console.log(loginUserMessage(""))      // print (just logged in)
 
-console.log(loginUserMessage("pranav"))        // print (undefined just logged in)
+// console.log(loginUserMessage("pranav"))        // print (undefined just logged in)
+
+// function calculateCartPrice(val1,val2,...num1){
+              //       OR
+function calculateCartPrice(...num1){           // rest operator or spread operator
+    return num1
+
+}
+
+// console.log(calculateCartPrice(200,400,500,600));
+
+const user = {
+    username: "Pranav",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "sam",                  // passing objects
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([200, 400, 100,600]));
