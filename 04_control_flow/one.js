ca6325ddfc1b45be951752248d1f59de8093aeb9
thePrnav <114599439+thePrnav@users.noreply.github.com>
2024-06-26 07:17:25 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,62 @@
+// if 
+
+// const isUserloggedIn = true
+
+// if(3 != 2){
+//     console.log("executed");
+
+// }
+
+// < , > , <= , >= , == , != , ===, !==
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// const temp = 41
+// if (temp === 41){
+//     console.log("less than 50");
+// }else{
+// console.log("temp is greater than 50");
+// }
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const score = 200
+
+// if (score > 100){
+
+//     let power = "fly"        // var power = "fly"   // const power = "fly"
+
+//     console.log(`User power: ${power}`);
+// }
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test");  // one linner :- implicit scope
+
+// if (balance > 500) console.log("test"), console.log("test2"); // multiple line scope (avoide)
+
+// if (balance < 500){
+//     console.log("less than 500");
+// }else if (balance < 750){
+//     console.log("less than 750");
+// }else if (balance < 900){
+//     console.log("less than 900");
+// }else{
+//     console.log("less than 1200")
+// }
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++node 04_control_flow/switch.js
+
+// const userLoggedIn = true
+// const debitCard = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true
+
+// if(userLoggedIn && debitCard && 2==3){
+//     console.log("Allow to buy course");
+// }
+
+// if(loggedInFromGoogle || loggedInFromEmail){
+//     console.log("Userlogged in");
+// }
